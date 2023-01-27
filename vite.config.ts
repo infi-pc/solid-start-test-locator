df30f36d72c895995b3cbc1f3a3b07146505c629
Michael Musil <infi.pc@gmail.com>
2023-01-27 13:31:19 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [
+    solid({
+      babel: {
+        plugins: [
+          [
+            "@locator/babel-jsx/dist",
+            {
+              env: "development",
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+});
